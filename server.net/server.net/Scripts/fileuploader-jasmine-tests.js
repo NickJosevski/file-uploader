6bea08d8e6a04a54e689f5bd9c6474be9e4ad45f
Nick Josevski <nickjosevski@gmail.com>
2012-02-13 22:26:51 +1100

    some more tests, tried a spec helper but it wasn't useful

--- server.net/server.net/Scripts/fileuploader-jasmine-tests.js
+++ server.net/server.net/Scripts/fileuploader-jasmine-tests.js
@@ -1,6 +1,5 @@
 ﻿describe("file-upload", function () {
     var uploader;
-    var somethignelse;
 
     beforeEach(function () {
         var temp = $("#temp-elements");
@@ -17,7 +16,7 @@
     });
 
     afterEach(function () {
-        $("#temp-elements").empty();
+        //$("#temp-elements").empty();
     });
 
     it("should have created a new uploader", function () {
@@ -26,7 +25,7 @@
 
     it("should create 4 spans, including a spinner when _addToList is called", function () {
 
-        uploader._addToList(1, 'bla');
+        uploader._addToList(1, 'file.name');
         var list = $('#file-uploader').find(".qq-upload-list");
         var childSpans = list.children().find("span");
         var spinner = list.children().find(".qq-upload-spinner");
@@ -34,6 +33,46 @@
         expect(list.length).toEqual(1);
         expect(childSpans.length).toEqual(4);
         expect(spinner.length).toEqual(1);
+
+        //remove:
+        $("#temp-elements").empty();
+    });
+
+    it("should have 3 remaining spans, with the sucess one visible when _onComplete is called with a success result", function () {
+        uploader._addToList(1, 'file.name');
+        uploader._onComplete(1, 'file.name', { success: true });
+
+        var list = $('#file-uploader').find(".qq-upload-list");
+        var children = list.children();
+        var childSpans = children.find("span");
+        var success = list.find(".qq-upload-success");
+        var size = list.find(".qq-upload-size");
+        var failed = list.find(".qq-upload-failed-text");
+
+        expect(list.length).toEqual(1);
+        expect(childSpans.length).toEqual(3);
+        expect(success.length).toEqual(1);
+        expect(size.length).toEqual(1);
+        expect(failed.length).toEqual(1);
+
+        //remove:
+        $("#temp-elements").empty();
+    });
+
+    it("should show failed correctly, when _onComplete is called with a success = false result", function () {
+        uploader._addToList(1, 'file.name');
+        uploader._onComplete(1, 'file.name', { success: false });
+
+        var list = $('#file-uploader').find(".qq-upload-list");
+        var failed = list.find(".qq-upload-fail");
+
+        expect(failed.length).toEqual(1);
+    });
+
+    it("should report progress on a call to _onProgress", function () {
+        //remove:
+        $("#temp-elements").empty();
+
     });
 }
 );
